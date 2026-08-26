12610df10f04e43874f208ab370548054f8b1c93
Enaitz <6enaitz6@gmail.com>
2026-08-26 16:01:22 +0200

    Weather improvement and minor changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from textual import work
 from rich.text import Text
 from textual.containers import Container
 
-disabled = False
+command = ["curl", "-s", "v2.wttr.in/Golada?m0&lang=es"]
 
 def parse_weather(original):
     lines = original.split('\n')
@@ -19,6 +19,29 @@ def parse_weather(original):
 
     new = lines[:cutIndex + 1]
 
+    new.pop(1)
+    new.append("└────────────────────────────────────────────────────────────────────────┘")
+
+    return '\n'.join(new)
+
+def parse_precipitations(original):
+    lines = original.split('\n')
+
+    cutIndex = 0
+    cutIndexTop = 0
+    for i, line in enumerate(lines):
+        if "6    12    18" in line:
+            cutIndexTop = i
+
+        if "│      ──────────────          ──────────────          ──────────────    │" in line:
+            cutIndex = i
+            break
+
+    new = lines[cutIndexTop-2:cutIndex -1]
+
+    new.insert(0, lines[0])
+    new.insert(1,lines[1])
+    new.insert(2, lines[3])
     new.append("└────────────────────────────────────────────────────────────────────────┘")
 
     return '\n'.join(new)
@@ -30,7 +53,7 @@ class Weather(wd.Static):
     @work(exclusive=True, thread=True)
     def load_weather(self) -> None:
         out = parse_weather(subprocess.run(
-            ["curl", "-s", "v2.wttr.in/?m0"],
+            command,
             capture_output=True, text=True
         ).stdout)
         self.app.call_from_thread(self.update, Text.from_ansi(out))
@@ -39,8 +62,23 @@ class WeatherContainer(Container):
     def compose(self) -> ComposeResult:
         yield Weather()
 
-# Real time clock on center
-class Clock_container(Container):
+class Rain(wd.Static):
+    def on_mount(self) -> None:
+        self.load_rain()
+
+    @work(exclusive=True, thread=True)
+    def load_rain(self) -> None:
+        out = parse_precipitations(subprocess.run(
+            command,
+            capture_output=True, text=True
+        ).stdout)
+        self.app.call_from_thread(self.update, Text.from_ansi(out))
+
+class RainContainer(Container):
+    def compose(self) -> ComposeResult:
+        yield Rain()
+
+class ClockContainer(Container):
     def compose(self) -> ComposeResult:
         yield Clock()
 
@@ -55,29 +93,37 @@ class Clock(wd.Digits):
 
 class Home(Container):
     def compose(self) -> ComposeResult:
-        yield Clock_container()
+        yield ClockContainer()
 
-class DashboardApp(App):
+class Dashboard(App):
     CSS = """
-    Clock_container {
-        align: center top;
-    }
+        ClockContainer {
+            align: center top;
+        }
 
-    Clock {
-        width: auto;
-        height: auto;
-    }
-    
-    WeatherContainer {
-        align: center middle;
-    }
-    
-    Weather {
-        width: auto;
-        height: auto;
-    }
-    
-        """
+        Clock {
+            width: auto;
+            height: auto;
+        }
+
+        WeatherContainer {
+            align: center middle;
+        }
+
+        Weather {
+            width: auto;
+            height: auto;
+        }
+        
+        RainContainer {
+            align: center middle;
+        }
+
+        Rain {
+            width: auto;
+            height: auto;
+        }
+            """
 
     BINDINGS = [("q", "quit", "Exit")]
     def compose(self) -> ComposeResult:
@@ -87,10 +133,12 @@ class DashboardApp(App):
                 yield Home()
             with wd.TabPane("Weather"):
                 yield WeatherContainer()
+            with wd.TabPane("Precipitations"):
+                yield RainContainer()
         yield wd.Footer()
 
         #event.button.id
 
 if __name__ == "__main__":
-    app = DashboardApp()
+    app = Dashboard()
     app.run()
